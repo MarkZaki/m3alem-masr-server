f6b50ba5f437fab3ef1bdba5dd8bae648a54f3ba
Mark Zaky <markzaky18@gmail.com>
2021-03-27 22:45:50 +0200

    error handle

--- routes/user.js
+++ routes/user.js
@@ -49,7 +49,7 @@ router.post("/login", async (req, res) => {
 	if (error) return res.status(400).send({ error: error.details[0].message });
 	// Find email
 	const user = await User.findOne({ email: req.body.email });
-	if (!user) return res.status(400).send("Email not found");
+	if (!user) return res.status(400).send({ error: "Email not found" });
 	// Check Password
 	const validPass = await bcrypt.compare(req.body.password, user.password);
 	if (!validPass)

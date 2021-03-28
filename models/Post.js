25a711140d02a296b82e3bdbdbc469858b2a3511
Mark Zaky <markzaky18@gmail.com>
2021-03-28 21:50:20 +0200

    date

--- models/Post.js
+++ models/Post.js
@@ -15,6 +15,10 @@ const postSchema = mongoose.Schema({
 	user: {
 		type: mongoose.Schema.Types.ObjectId,
 		ref: "User"
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now
 	}
 });
 module.exports = mongoose.model("Post", postSchema);
